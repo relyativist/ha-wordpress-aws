9b78de7e722237976c449b3ec7e071bb231e683a
Vyacheslav Yarkin <yarkin.vyacheslav@mail.ru>
2021-12-10 02:26:37 +0300

    subnets plan check works

--- vpc.tf
+++ vpc.tf
@@ -8,3 +8,46 @@ resource "aws_vpc" "base-vpc" {
         Name = "base-vpc"
     }
 }
+
+resource "aws_subnet" "base-subnet-public-1" {
+    vpc_id = aws_vpc.base-vpc.id
+    cidr_block ="10.0.1.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "eu-central-1a"
+    tags = {
+        Name = "base-subnet-public-1"
+    }
+
+}
+
+resource "aws_subnet" "base-subnet-public-2" {
+    vpc_id = aws_vpc.base-vpc.id
+    cidr_block ="10.0.2.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "eu-central-1b"
+    tags = {
+        Name = "base-subnet-public-2"
+    }
+
+}
+
+resource "aws_subnet" "base-subnet-private-1" {
+    vpc_id = aws_vpc.base-vpc.id
+    cidr_block ="10.0.3.0/24"
+    map_public_ip_on_launch = "false"
+    availability_zone =  "eu-central-1c"
+    tags = {
+        Name = "base-subnet-private-1"
+    }
+}
+
+resource "aws_subnet" "base-subnet-private-2" {
+    vpc_id = aws_vpc.base-vpc.id
+    cidr_block ="10.0.5.0/24"
+    map_public_ip_on_launch = "false"
+    availability_zone =  "eu-central-1a"
+    tags = {
+        Name = "base-subnet-private-2"
+    }
+}
+
